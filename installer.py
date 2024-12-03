b7c796213decbc9ea12f23b0c631b0dc8144c1d1
SysAlex <burenin_alexander@list.ru>
2024-12-03 18:51:02 +0300

    First commmit!!!

--- installer.py
+++ installer.py
@@ -9,17 +9,7 @@ encoder = "libx264"
 
 
 
-# Список платформ для стрима
-platforms = [
-    {"url": "rtmp://live.twitch.tv/app/live_1128390212_GAt9MBDQ2RUg2sQVwhuwm0AdtOOw5V",
-     "key": "live_1128390212_GAt9MBDQ2RUg2sQVwhuwm0AdtOOw5V",
-     "bitrate": "5000k",
-     "encoder": encoder},
-    {"url": "rtmp://livepush.trovo.live/live/",
-     "key": "73846_116420352_116420352?bizid=73846&txSecret=cdd812c92cc71b7aafe2dd9dd13c3a1b&txTime=79885983&cert=52376e54effbd2f1a0bad52823644940&certTime=66bc5683&flag=txcloud_116420352_116420352&timeshift_bps=0%7C2500%7C1500&timeshift_dur=43200&txAddTimestamp=4&tp_code=1723618947&tp_sign=355285362&dm_sign=760340275&pq_sn=804972357&txHost=livepush.trovo.live",
-     "bitrate": "2000k",
-     "encoder": encoder},
-]
+
 
 
 # Функция запуска стрима через FFmpeg
